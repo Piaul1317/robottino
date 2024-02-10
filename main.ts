403a01bc5fff508a8756727cabcce4a3a8c3fecd
Piaul1317 <159622179+Piaul1317@users.noreply.github.com>
2024-02-10 14:19:23 +0100

    Messa modifica per derapata

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-function robot_Avanti() {
-    
+function robot_Avanti () {
     if (controllo != 70) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, velocita)
         mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, velocita)
@@ -14,11 +13,8 @@ function robot_Avanti() {
             . . # . .
             `)
     }
-    
 }
-
-function robot_destra() {
-    
+function robot_destra () {
     if (controllo != 67) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, velocitaRotazione)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Back, velocitaRotazione)
@@ -33,11 +29,8 @@ function robot_destra() {
             . . # . .
             `)
     }
-    
 }
-
-function robot_Ferma() {
-    
+function robot_Ferma () {
     if (controllo != 0) {
         controllo = 0
         mecanumRobotV2.state()
@@ -49,11 +42,22 @@ function robot_Ferma() {
             . . . . .
             `)
     }
-    
 }
-
-function robot_sinistra() {
-    
+function derapata_sinistra () {
+    if (controllo != 90) {
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, velocita)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Back, velocita)
+        controllo = 90
+        basic.showLeds(`
+            # . # # #
+            # # . . #
+            # # # . #
+            . . . . .
+            . . . . .
+            `)
+    }
+}
+function robot_sinistra () {
     if (controllo != 68) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Back, velocitaRotazione)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, velocitaRotazione)
@@ -68,11 +72,8 @@ function robot_sinistra() {
             . . # . .
             `)
     }
-    
 }
-
-function robot_ShiftSinistra() {
-    
+function robot_ShiftSinistra () {
     if (controllo != 13) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Back, velocita)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, velocita)
@@ -87,11 +88,22 @@ function robot_ShiftSinistra() {
             . . # . .
             `)
     }
-    
 }
-
-function robot_ShiftDestra() {
-    
+function derapata_destra () {
+    if (controllo != 8) {
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, velocita)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, velocita)
+        controllo = 8
+        basic.showLeds(`
+            # # # . #
+            # . . # #
+            # . # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+}
+function robot_ShiftDestra () {
     if (controllo != 22) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, velocita)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Back, velocita)
@@ -106,11 +118,8 @@ function robot_ShiftDestra() {
             . . # . .
             `)
     }
-    
 }
-
-function robot_Indietro() {
-    
+function robot_Indietro () {
     if (controllo != 21) {
         mecanumRobotV2.Motor(LR.Upper_left, MD.Back, velocita)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Back, velocita)
@@ -125,15 +134,16 @@ function robot_Indietro() {
             . . # . .
             `)
     }
-    
 }
-
 let controllo = 0
+let velocitaRotazione = 0
 let velocita = 0
 velocita = 50
-let velocitaRotazione = 20
+velocitaRotazione = 20
 irRemote.connectInfrared(DigitalPin.P0)
-basic.forever(function on_forever() {
+serial.redirectToUSB()
+basic.forever(function () {
+    serial.writeValue("ir:", irRemote.returnIrButton())
     if (irRemote.returnIrButton() == 70) {
         robot_Avanti()
     } else if (irRemote.returnIrButton() == 21) {
@@ -146,8 +156,11 @@ basic.forever(function on_forever() {
         robot_ShiftDestra()
     } else if (irRemote.returnIrButton() == 22) {
         robot_ShiftSinistra()
+    } else if (irRemote.returnIrButton() == 90) {
+        derapata_destra()
+    } else if (irRemote.returnIrButton() == 8) {
+        derapata_sinistra()
     } else {
         robot_Ferma()
     }
-    
 })
